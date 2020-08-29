9275eed6e18905d2341cc76125c7cc5bdb7a075f
Ryan B Fernandes <ryan.fernandes@saude.ce.gov.br>
2020-08-28 23:18:03 -0300

    Adicionando classes criadas pelo scafold para testes de entidades. Caso eu fosse fazer algumas gostaria de ter criado apenas as que representam a classe de serviço pois é onde estária implementado o negocio

---
+++ backend/src/test/groovy/backend/PriceSpec.groovy
@@ -0,0 +1,18 @@
+package backend
+
+import grails.testing.gorm.DomainUnitTest
+import spock.lang.Specification
+
+class PriceSpec extends Specification implements DomainUnitTest<Price> {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
+}
