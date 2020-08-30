768f75f1026f5fc52e0a4641740653f3b7d7615a
ryan.fernandes <ryan.fernandes@saude.ce.gov.br>
2020-08-29 22:56:41 -0300

    Mlehorando formato para adicionar 'FAKES DATES' MOCK. de dados ao startar a aplicacao

--- backend/grails-app/init/backend/BootStrap.groovy
+++ backend/grails-app/init/backend/BootStrap.groovy
@@ -4,31 +4,38 @@ import groovy.time.TimeCategory
 
 class BootStrap {
     
-    def companys = []
+    CompanyService companyService
+    StockService stockService
     def init = { servletContext ->
-        Company railroad = BootStrap.createCompany("railroad","land vehicles")
-        Company carriers = BootStrap.createCompany("carriers","land vehicles")
-        Company navigation = BootStrap.createCompany("navigation","floating vehicles")
+    /* Este formato de inserção por create-drop torna bem lento o desenvolvimento.
+     Preferiria ter feito de alguma outra forma. Por exemplo com dataSet ja definido para um ambiente
+     de teste. Uma instancia de um BANCO com os dados já existentes... Apesar de eu tentar melhora no insert com BATCH
+     */
+        Company railroad = BootStrap.createCompany("railroad".toUpperCase(),"land vehicles".toUpperCase());
+        Stock[] stocksRailroad = BootStrap.createStockQuotes(railroad);
+        stockService.save(stocksRailroad);
 
-        companys.push(railroad)
-        companys.push(carriers)
-        companys.push(navigation)
+        /*
+        Company carriers = BootStrap.createCompany("carriers".toUpperCase(),"land vehicles".toUpperCase());
+        Stock[] stocksCarriers = BootStrap.createStockQuotes(carriers);
+        stockService.save(stocksCarriers);
+        companyService.save(BootStrap.createCompany("navigation".toUpperCase(),"floating vehicles".toUpperCase()))
+        */
     }
     def destroy = {}
 
-    public static Company createCompany(String name, String segment) {
+    private static Company createCompany(String name, String segment) {
         Company company = new Company();
         company.name = name;
         company.segment = segment
-        company.stocks = BootStrap.createStockQuotes()
-
-        return company;
+        BootStrap.createStockQuotes(company)
+        return company
     }
 
-    public static Stock[] createStockQuotes() {
+    private static Stock[] createStockQuotes(Company company ) {
         def stocks = [];
         Date today = new Date()
-        today.set(hourOfDay: 18, minute: 0, second: 0)
+        today.set(hourOfDay: 18, minute: 1, second: 0)
         Date last30Days = today.plus(-29)
         Date init = last30Days;
         BootStrap.setFirstHours(init)
@@ -47,20 +54,20 @@ class BootStrap {
                 }
                 if(init < today) {
                     Stock stock = new Stock()
-                    stock.price = new Price()
-                    stock.price.date = init;
+                    stock.datePrice = init;
                     BigDecimal random = BootStrap.generateRandomBigDecimalFromRange(
                         new BigDecimal(0.01).setScale(2, BigDecimal.ROUND_HALF_UP),
                         new BigDecimal(500.00).setScale(2, BigDecimal.ROUND_HALF_UP)
                     );
-                    stock.price.value = random
+                    stock.price = random
+                    stock.company = company
                     stocks.push(stock)
 
                     def hours = init.getHours();
                     init = init+1.minute
                     def hoursChange = hours != init.getHours()
                     
-                    def isLastMinute = init.getHours() == 18
+                    def isLastMinute = init.getHours() == 18 && init.getMinutes() == 1
                     if(isLastMinute) {
                         isDayChange = true
                     } 
@@ -71,11 +78,11 @@ class BootStrap {
         return stocks;
     }
 
-    public static void setFirstHours(Date date) {
+    private static void setFirstHours(Date date) {
         date.set(hourOfDay: 10, minute: 0, second: 0);
     }
 
-    public static BigDecimal generateRandomBigDecimalFromRange(BigDecimal min, BigDecimal max) {
+    private static BigDecimal generateRandomBigDecimalFromRange(BigDecimal min, BigDecimal max) {
         BigDecimal randomBigDecimal = min.add(new BigDecimal(Math.random()).multiply(max.subtract(min)));
         return randomBigDecimal.setScale(2,BigDecimal.ROUND_HALF_UP);
     }
